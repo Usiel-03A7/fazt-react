1609bfa84c7190c100482451ddecbe6c8823566c
Usiel-03A7 <shebastyanmail.com>
2023-01-03 11:14:50 -0600

    creando el primer componente root usando DOM y render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hola react</h1>)
 
+/**
+ * 
+ * Funcion que regresa un componente el cual
+ * tiene un padre y es retornado para ser usado al llamarlo 
+ */
+function Gretting() {
+  return (
+     <div>
+       <h1>este es un componente</h1>
+     </div>
+  )
+}
+/**
+ * root es la variable principal que se declaro con el DOM.
+ * render es una funcion del root para mostrar lo que se ponga
+ * dentro del parentesis ---> (here)
+ */
+root.render(
+  <div>
+    <Gretting/>
+  </div>
+)
